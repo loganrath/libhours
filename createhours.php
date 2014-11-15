a75a40cf40eb1b3965f03587dcf3c337e01565ef
loganrath <lrath@brockport.edu>
2014-11-15 17:22:08 -0500

    Update createhours.php
    fixed final comma showing up

--- createhours.php
+++ createhours.php
@@ -2,23 +2,23 @@
 
 $date = '2014-11-01';
 while ($date <= '2014-12-31') {
-	echo makeJSON($date);
+	echo rtrim(makeJSON($date), ",");
 	$date = date('Y-m-d', strtotime($date . ' + 1 day'));
 }
 
-function makeJSON($currdate) {
-	$w = date('w', strtotime($currdate));
-	if ($currdate == '2014-11-26') {$libhours='7:45am - \r\n5pm'; $bg='cyan !important';}
-	elseif ($currdate >= '2014-11-27' && $currdate <= '2014-11-29') {$libhours = "CLOSED"; $bg='red !important';}
+function makeJSON($date) {
+	$w = date('w', strtotime($date));
+	if ($date == '2014-11-26') {$libhours='7:45am - \r\n5pm'; $bg='cyan !important';}
+	elseif ($date >= '2014-11-27' && $date <= '2014-11-29') {$libhours = "CLOSED"; $bg='red !important';}
 	elseif ($w==0) {$libhours = '12pm -\r\n1:30am'; $bg='#8DC53E !important';}
 	elseif ($w==6) {$libhours = '12pm -\r\n8pm'; $bg='#8DC53E !important';}
 	else {$libhours = '7:45am -\r\n1:30am'; $bg='#8DC53E !important';}
-echo'
+return '
 {
     "allDay": true,
     "title": "'.$libhours.'",
-    "start": "'.$currdate.'",
-    "end": "'.date('Y-m-d', strtotime($currdate . ' + 1 day')).'",
+    "start": "'.$date.'",
+    "end": "'.date('Y-m-d', strtotime($date . ' + 1 day')).'",
     "borderColor": "white",
     "backgroundColor": "'.$bg.'"
   },';
