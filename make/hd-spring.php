6f20d318f63cdf8e8e8066335b01bc25ef8dd80f
loganrath <lrath@brockport.edu>
2015-01-25 13:33:17 -0500

    Update hd-spring.php

--- make/hd-spring.php
+++ make/hd-spring.php
@@ -14,6 +14,7 @@ $date = date('Y-m-d', strtotime($date . ' + 1 day'));
 function makeJSON($date) {
 $w = date('w', strtotime($date));
 if ($w==0) {$refstart = $date.'T12:00:00'; $refend = $date.'T20:00:00';}
+elseif ($w==5) {$refstart = $date.'T08:00:00'; $refend = $date.'T16:00:00';}
 elseif ($w==6) {$refstart = $date.'T14:00:00'; $refend = $date.'T18:00:00';}
 else {$refstart = $date.'T08:00:00'; $refend = $date.'T20:00:00';}
 return '
