b65bf7c6d5bc9cb6b757f2c8c8b95ab6b5840069
loganrath <lrath@brockport.edu>
2015-01-25 13:23:38 -0500

    Update hd-spring.php

--- make/hd-spring.php
+++ make/hd-spring.php
@@ -2,26 +2,26 @@
 ####################################
 ##### Begin Configuration ##########
 # Start date of string to populate #
-$date = '2015-01-05';
+$date = '2015-01-26';
 ## End date of string to populate ##
-$end_date = '2015-01-24';
+$end_date = '2014-12-31';
 ###### End Configuration ##########
 ####################################
 while ($date <= $end_date) {
-echo makeJSON($date);
+echo makeJSON($date), ",";
 $date = date('Y-m-d', strtotime($date . ' + 1 day'));
 }
 function makeJSON($date) {
 $w = date('w', strtotime($date));
-	if ($w==0 || $w==6) return '';
-else {$refstart = $date.'T14:00:00'; $refend = $date.'T16:00:00';}
+if ($w==0 || $w==6) {$refstart = $date.'T12:00:00'; $refend = $date.'T18:00:00';}
+else {$refstart = $date.'T10:00:00'; $refend = $date.'T20:00:00';}
 return '
 {
-"title": "IT Desk",
+"title": "Research Help",
 "start": "'.$refstart.'",
 "end": "'.$refend.'",
 "borderColor": "white",
-"backgroundColor": "orange !important;"
+"backgroundColor": "#5BC0DE !important;"
 },';
 }
 ?>
